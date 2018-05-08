1972c5304abc021b83061e0f5044888f5b2dbd5e
nissandjac@gmail.com <nissandjac@gmail.com>
2018-05-08 15:45:07 -0800

    solve conflict

--- Spatial MSE/test_stuff.R
+++ Spatial MSE/test_stuff.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 reps <- obj$report() # Saves the last round
 
 reps$ans
@@ -19,25 +18,3 @@ plot(reps$Catch/reps$SSB)
 
 
 
-=======
-reps <- obj$report() # Saves the last round
-
-reps$ans
-
-reps$ans_catch
-reps$ans_catchcomp
-reps$ans_psel
-reps$ans_SDR
-reps$ans_survcomp
-reps$ans_survey
-
-plot(reps$Fyear)
-
-plot(reps$Catch/reps$SSB)
-
-# Plot all the selectivities 
-
-
-
-
->>>>>>> baeb649a73da2a87886096ac53b590dddb82de24
