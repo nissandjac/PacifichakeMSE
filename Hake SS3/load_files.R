c49113d488b484a7d2fd091326b71d932422fb4d
nissandjac@gmail.com <nissandjac@gmail.com>
2019-04-24 09:07:21 -0700

    fixes to stock assessment

---
+++ Hake SS3/load_files.R
@@ -0,0 +1,12 @@
+## Load the required scripts and packages 
+require(TMB)
+#library(TMBdebug)
+require(ggplot2)
+
+
+source('plotValues.R')
+source('getUncertainty.R')
+source('load_data.R')
+source('ylimits.R')
+source('getParameters.R')
+source('Check_Identifiable_vs2.R')
