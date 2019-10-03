80adb7e866c8399340d15b52da3290070f6c6e91
nissandjac@gmail.com <nissandjac@gmail.com>
2019-10-03 16:48:51 -0700

    OM output changes

--- Spatial MSE/run_multiple_MSEs.R
+++ Spatial MSE/run_multiple_MSEs.R
@@ -1,65 +1,67 @@
 ###### Run the HAKE MSE ####### 
-run_multiple_MSEs <- function(simyears = NULL,seeds = 12345, TAC = 1, df = NA){
-require(ggplot2)
+run_multiple_MSEs <- function(simyears = NULL,seeds = 12345, TAC = 1, df = NA,
+                                      cincrease = 0, mincrease = 0){
+  require(ggplot2)
   
-if(is.null(simyears)){
-  print('Number of years to simulate not specified. Simulating 30 years into the future')
-  simyears <- 30
-}
-  
-  
-#   if(is.na(moveparms[1])){
-# df <- load_data_seasons(move = TRUE,
-#                         nseason = 4, nspace = 2)
-#   }else{
-#     if(length(moveparms) != 2){
-#       stop('Wrong number of movement parameters')
-#     }
-#     df <- load_data_seasons(move =TRUE,
-#                             nseason = 4, nspace = 2,movemaxinit = moveparms[1],movefiftyinit = moveparms[2])
-#   }
-
-time <- 1
-yrinit <- df$nyear
-
-year.future <- c(df$years,(df$years[length(df$years)]+1):(df$years[length(df$years)]+simyears))
-N0 <- NA
-sim.data <- run.agebased.true.catch(df,seeds)
-simdata0 <- sim.data # The other one is gonna get overwritten. 
-
-F40.save<- array(NA,simyears)
-
-# Save some stuff
-SSB.save <- list()
-R.save <- list()
-Catch.save <- list()
-S.year.future <- seq(2019,2019+simyears, by = df$nsurvey)
-# Save som OM stuff 
-
-
-# Save the estimated parameters from the EM (exclude time varying) 
-parms.save <- array(NA, dim = c(simyears, 4))
-
-# Before the MSE starts 
-F0.save <- df$fmort
-years <- df$years
-model.save <- list()
-
-## 
-
-SSB.test.om <- list() # Test if SSB is the same in the OM
-start.time <- Sys.time()
-df0 <- df # For debugging
-mconverge <- rep(NA, simyears)
-
-for (time in 1:simyears){
-  
-
-  year <- yrinit+(time-1)
-  #print(year.future[year])
+  if(is.null(simyears)){
+    print('Number of years to simulate not specified. Simulating 30 years into the future')
+    simyears <- 30
+  }
   
   
-  if (time > 1){
+  #   if(is.na(moveparms[1])){
+  # df <- load_data_seasons(move = TRUE,
+  #                         nseason = 4, nspace = 2)
+  #   }else{
+  #     if(length(moveparms) != 2){
+  #       stop('Wrong number of movement parameters')
+  #     }
+  #     df <- load_data_seasons(move =TRUE,
+  #                             nseason = 4, nspace = 2,movemaxinit = moveparms[1],movefiftyinit = moveparms[2])
+  #   }
+  
+  time <- 1
+  yrinit <- df$nyear
+  
+  year.future <- c(df$years,(df$years[length(df$years)]+1):(df$years[length(df$years)]+simyears))
+  N0 <- NA
+  sim.data <- run.agebased.true.catch(df,seeds)
+  simdata0 <- sim.data # The other one is gonna get overwritten. 
+  
+  F40.save<- array(NA,simyears)
+  
+  # Save some stuff
+  SSB.save <- list()
+  R.save <- list()
+  Catch.save <- list()
+  S.year.future <- seq(2019,2019+simyears, by = df$nsurvey)
+  # Save som OM stuff 
+  
+  
+  # Save the estimated parameters from the EM (exclude time varying) 
+  parms.save <- array(NA, dim = c(simyears, 4))
+  
+  # Before the MSE starts 
+  F0.save <- df$fmort
+  years <- df$years
+  model.save <- list()
+  
+  ## 
+  
+  SSB.test.om <- list() # Test if SSB is the same in the OM
+  start.time <- Sys.time()
+  df0 <- df # For debugging
+  mconverge <- rep(NA, simyears)
+  df0 <- df
+  
+  for (time in 1:simyears){
+    
+    
+    year <- yrinit+(time-1)
+    #print(year.future[year])
+    
+    
+    if (time > 1){
       
       if(sum(year.future[year] == S.year.future)>0){
         df$flag_survey <- c(df$flag_survey,1)
@@ -80,10 +82,12 @@ for (time in 1:simyears){
       df$years <- year.future[1:year]
       df$nyear <- length(df$years)
       #df$tEnd <- df$tEnd+1 # Just run one more year in subsequent runs
-      df$wage_catch <- df.new$wage_catch
-      df$wage_survey <- df.new$wage_survey
-      df$wage_mid <- df.new$wage_mid
-      df$wage_ssb <- df.new$wage_ssb
+      
+      df$wage_catch <- cbind(df.new$wage_catch,df.new$wage_catch[,1])
+      df$wage_survey <- cbind(df.new$wage_survey,df.new$wage_survey[,1])
+      df$wage_mid <- cbind(df.new$wage_mid,df.new$wage_mid[,1])
+      df$wage_ssb <- cbind(df.new$wage_ssb,df.new$wage_ssb[,1])
+      
       df$Catch <- c(df$Catch, Fnew[[1]])
       
       ## Add a survey if catches are 0 
@@ -107,11 +111,56 @@ for (time in 1:simyears){
       ### Add movement to the new years
       move.tmp <- array(0, dim = c(df$nspace,df$nage, df$nseason, df$nyear))
       move.tmp[,,,1:df$nyear-1] <- df$movemat
-      move.tmp[,,,df$nyear] <- df$movemat[,,,df$nyear-1]
+      
+      # Add increasing movement due to climate
+      nspace <- df$nspace
+      nage <- df$nage
+      nseason <- df$nseason
+      age <- df$age
+      
+      
+      movenew <-array(0, dim = c(nspace, nage, nseason)) # Chances of moving in to the other grid cell 
+      movemax <- df$movemax
+      movefifty <- df$movefifty
+      
+      if(time == 2){
+        movemaxtmp <- (movemax[1]+cincrease)
+        df$moveout <- df$moveout-mincrease
+      }else{
+        movemaxtmp <- movemaxtmp+cincrease
+        df$moveout <- df$moveout-mincrease
+        
+      }
+      
+      if(movemaxtmp >0.9){
+        movemaxtmp <- 0.9 # Not moving more than 90% out 
+        
+        # (stop(paste(time,'at max movement')))
+      }
+      
+      if(df$moveout <= 0.5){
+        df$moveout <- 0.5
+      }
+      
+      for(j in 1:nspace){
+        for(i in 1:nseason){
+          movenew[j,,i] <- movemaxtmp/(1+exp(-df$moveslope*(age-movefifty)))
+        }
+      }
+      
+      if(nseason == 4){ # For the standard model
+        movenew[,1:2,] <- 0 # Recruits and 1 year olds don't move
+        
+        movenew[1,4:nage,1:3] <- df$movesouth # Don't move south during the year
+        movenew[1,3:nage,nseason] <- df$moveout
+        movenew[2,3:nage,nseason] <- df$movesouth
+      }
+      
+      move.tmp[,,,df$nyear] <- movenew
       df$movemat <- move.tmp
       #df$years <- c(df$years,df$years[length(df$years)]+1)
       
-      
+      #print(df$moveout)
       sim.data <- run.agebased.true.catch(df, seeds)
       
     }
@@ -137,7 +186,7 @@ for (time in 1:simyears){
     #   PSEL = PSEL
     # )
     # 
-  
+    
     parms <- getParameters_OM(trueparms = TRUE, df = df)
     
     ##  Create a data frame to send to runHakeassessment 
@@ -147,7 +196,7 @@ for (time in 1:simyears){
     parms.new <- parms
     
     if(time == 1){
-      F0 <- rowSums(sim.data$Fout)/2
+      F0 <- rowSums(sim.data$Fout)
       Rdev <- parms$Rin
     }else{
       F0 <- c(F0,0.2)
@@ -160,7 +209,6 @@ for (time in 1:simyears){
     }
     
     parms.new$F0 <- F0#rowSums(sim.data$Fsave, na.rm = TRUE)
-    
     #parms.new$F0[df$Catch == 0] <- 0
     
     parms.new$Rin <- Rdev#parms.new$Rin[1:(length(parms.new$Rin)-1)]
@@ -199,143 +247,142 @@ for (time in 1:simyears){
     system.time(opt<-nlminb(obj$par,obj$fn,obj$gr,lower=lower,upper=upper,
                             control = list(iter.max = 1e6, 
                                            eval.max = 1e6))) # If error one of the random effects is unused
-
+    
     
     if(opt$convergence != 0){
       print(paste('year',df$years[length(df$years)], 'has convergence issues, double checking parameters'))
       
       xx<- Check_Identifiable_vs2(obj)
       
-      if(xx[[1]] == 'model not converging'){
-        mconverge[time] <- 1
-        
-        if(sum(mconverge, na.rm = TRUE)>1){
-          print('Convergence is bad. Stopping simulation')
-          return(df.ret= NA)
-        }
-        
-      }else{
-        mconverge[time] <- 0
-      }
+      # if(xx[[1]] == 'model not converging'){
+      #   mconverge[time] <- 1
+      #   
+      #   if(sum(mconverge, na.rm = TRUE)>1){
+      #     print('Convergence is bad. Stopping simulation')
+      #     return(df.ret= NA)
+      #   }
+      #   
+      # }else{
+      #   mconverge[time] <- 0
+      # }
     }else{
       mconverge[time] <- 0
     }
-  
-  reps <- obj$report()
-  
-  SSB <- reps$SSB
-  Fyear <- reps$Fyear
-  N <- reps$N_beg
-  Catch <- reps$Catch
-  R <- reps$R
-  
-  # plot(SSB)
-  # lines(rowSums(sim.data$SSB))
-
-  # # lines(rowSums(sim.data$SSB), col = 'red')
-  # # #Uncertainty
-  
-
-  if(time == simyears){
-    rep <- sdreport(obj)
-    sdrep <- summary(rep)
-    rep.values<-rownames(sdrep)
-    nyear <- df$tEnd
-
-    R <- data.frame(name = sdrep[rep.values == 'R',1])
     
-    SSB <- data.frame(name = sdrep[rep.values == 'SSB',1])
-    SSB$SE <- sdrep[rep.values == 'SSB',2]
-    SSB$min <- SSB$name-2*SSB$SE
-    SSB$max <- SSB$name+2*SSB$SE
-    SSB$year <- df$years
+    reps <- obj$report()
     
-    if(is.na(df$move)){
-      p1 <- plotValues(SSB, data.frame(x=df$years,y = sim.data$SSB), 'SSB')  
-    }else{
-      p1 <- plotValues(SSB, data.frame(x=df$years,y = rowSums(sim.data$SSB)), 'SSB')
+    SSB <- reps$SSB
+    Fyear <- reps$Fyear
+    N <- reps$N_beg
+    Catch <- reps$Catch
+    R <- reps$R
+    
+    plot(sim.data$Catch)
+    lines(apply(sim.data$Catch.quota,1,sum))
+    
+    # # lines(rowSums(sim.data$SSB), col = 'red')
+    # # #Uncertainty
+    
+    
+    if(time == simyears){
+      rep <- sdreport(obj)
+      sdrep <- summary(rep)
+      rep.values<-rownames(sdrep)
+      nyear <- df$tEnd
+      
+      R <- data.frame(name = sdrep[rep.values == 'R',1])
+      
+      SSB <- data.frame(name = sdrep[rep.values == 'SSB',1])
+      SSB$SE <- sdrep[rep.values == 'SSB',2]
+      SSB$min <- SSB$name-2*SSB$SE
+      SSB$max <- SSB$name+2*SSB$SE
+      SSB$year <- df$years
+      
+      if(is.na(df$move)){
+        p1 <- plotValues(SSB, data.frame(x=df$years,y = sim.data$SSB), 'SSB')  
+      }else{
+        p1 <- plotValues(SSB, data.frame(x=df$years,y = rowSums(sim.data$SSB)), 'SSB')
+      }
+      
+      print(p1)
+      SSB.hes <- SSB
+      SSB <- SSB$name
+      
     }
     
-    print(p1)
-    SSB.hes <- SSB
-    SSB <- SSB$name
+    
+    Vreal <- sum(sim.data$N.save.age[,df$nyear,,df$nseason]*
+                   matrix(rep(df$wage_catch[,df$nyear-1],df$nspace), ncol = df$nspace)*t(sim.data$Fsel[df$nyear,,]))
+    
+    Nend <- N[,dim(N)[2]]
+    Fnew <- getRefpoint(opt$par, df,SSBy = SSB[length(SSB)], Fin=Fyear[length(Fyear)], Nend, TAC = TAC,
+                        Vreal)
+    #Fnew <- 0.3
+    # Update the data data frame
+    # if(Fnew[[1]] == 1){
+    #   stop('fishery closed')
+    # }
+    # 
+    Ntmp <- sim.data$Nout
+    
+    
+    # Save some EM stuff in the last year 
+    SSB.save[[time]] <- SSB
+    R.save[[time]] <- N[1,]
+    F40.save[time] <- Fnew[[2]]
+    Catch.save[[time]] <- Catch
+    
+    # And the fishing mortality
+    #F0.save <- Fnew
+    
+    #  print(year.future[year])
+    #SSB.test.om[[time]] <- rowSums(sim.data$SSB)
+    
+    ### Include the parameters needed to calculate SSB0 
+    parms.save[time, ] <- exp(opt$par)[1:4]
+    
+    # year = df$years[time]
+    
     
   }
- 
   
-  Vreal <- sum(sim.data$N.save.age[,df$nyear,,df$nseason]*
-                 matrix(rep(df$wage_catch[,df$nyear-1],df$nspace), ncol = df$nspace)*t(sim.data$Fsel[df$nyear,,]))
-  
-  Nend <- N[,dim(N)[2]]
-  Fnew <- getRefpoint(opt$par, df,SSBy = SSB[length(SSB)], Fin=Fyear[length(Fyear)], Nend, TAC = TAC,
-                      Vreal)
-  #Fnew <- 0.3
-  print(paste('new quota = ',Fnew[[1]]))
-  # Update the data data frame
-  # if(Fnew[[1]] == 1){
-  #   stop('fishery closed')
-  # }
-  # 
-  Ntmp <- sim.data$Nout
+  nms <- unlist(strsplit(names(opt$par), split = 'log')[1:4])[c(2,4,6,8)]
+  names(parms.save) <- nms
   
   
-  # Save some EM stuff in the last year 
-  SSB.save[[time]] <- SSB
-  R.save[[time]] <- N[1,]
-  F40.save[time] <- Fnew[[2]]
-  Catch.save[[time]] <- Catch
+  end.time <- Sys.time()
+  time.taken <- end.time - start.time
+  print(time.taken)
   
-  # And the fishing mortality
-  #F0.save <- Fnew
+  #  Catch per country per year 
+  Catch.year <- apply(sim.data$Catch.save.age, FUN = sum, MARGIN = c(2,3))
   
-  #  print(year.future[year])
-  #SSB.test.om[[time]] <- rowSums(sim.data$SSB)
+  ## Calculate the average age 
+  source('calcMeanAge.R')
   
-  ### Include the parameters needed to calculate SSB0 
-  parms.save[time, ] <- exp(opt$par)[1:4]
+  #dev.off()
+  amc <- data.frame(year = year.future[1:year], 
+                    amc.can = calcMeanAge(sim.data$age_comps_catch_space[,,1], df$age_maxage),
+                    amc.US  = calcMeanAge(sim.data$age_comps_catch_space[,,2], df$age_maxage),
+                    amc.tot = calcMeanAge(sim.data$age_catch, df$age_maxage))
   
-    
-    
+  ams <- data.frame(year = year.future[1:year], 
+                    ams.can = calcMeanAge(sim.data$age_comps_country[,,1], df$age_maxage),
+                    ams.US  = calcMeanAge(sim.data$age_comps_country[,,2], df$age_maxage),
+                    ams.tot = calcMeanAge(sim.data$age_comps_surv, df$age_maxage))
   
-}
-
-nms <- unlist(strsplit(names(opt$par), split = 'log')[1:4])[c(2,4,6,8)]
-names(parms.save) <- nms
+  df.ret <- list(Catch = Catch.year, 
+                 Catch.quota = sim.data$Catch.quota,# All output is from the OM 
+                 SSB = sim.data$SSB, 
+                 SSB.mid = sim.data$SSB.all[,3,],
+                 SSB.hes = SSB.hes,
+                 Survey.om = sim.data$survey,
+                 F0 = apply(sim.data$Fout,c(1,3),sum),
+                 parms = parms.save,
+                 ams = ams,
+                 amc = amc,
+                 V = sim.data$V.save
+  )
   
-
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-print(time.taken)
-
-
-## Calculate the average age 
-source('calcMeanAge.R')
-#dev.off()
-amc <- data.frame(year = year.future[1:year], 
-                  amc.can = calcMeanAge(sim.data$age_comps_catch_space[,,1], df$age_maxage),
-                  amc.US  = calcMeanAge(sim.data$age_comps_catch_space[,,2], df$age_maxage),
-                  amc.tot = calcMeanAge(sim.data$age_catch, df$age_maxage))
-
-ams <- data.frame(year = year.future[1:year], 
-                  ams.can = calcMeanAge(sim.data$age_comps_country[,,1], df$age_maxage),
-                  ams.US  = calcMeanAge(sim.data$age_comps_country[,,2], df$age_maxage),
-                  ams.tot = calcMeanAge(sim.data$age_comps_surv, df$age_maxage))
-
-df.ret <- list(Catch = sim.data$Catch, 
-               Catch.quota = sim.data$Catch.quota,# All output is from the OM 
-               Catch.save.age = sim.data$Catch.save.age,
-               Catch.quota.N = sim.data$Catch.quota.N,
-               SSB = sim.data$SSB, 
-             #  V= sim.data$V.save,
-               SSB.mid = sim.data$SSB.all[,3,],
-               SSB.hes = SSB.hes,
-               Survey.om = sim.data$survey,
-               F0 = sim.data$Fsave,
-               parms = parms.save,
-               ams = ams,
-               amc = amc,
-               mconverge
-               )
-
-return(df.ret)
+  return(df.ret)
 }
