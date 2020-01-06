c5d53a69deaf9ddbc00263110350d68ffa3abca3
Kristin Marshall <kristin.marshall@noaa.gov>
2020-01-06 09:24:25 -0800

    survey frequency scenarios

---
+++ Spatial MSE/run_MSE_surveys.R
@@ -0,0 +1,111 @@
+###### Initialize the operating model ###### 
+library(TMB)
+
+setwd(paste(getwd(),'/Spatial MSE',sep=""))
+
+compile("runHakeassessment.cpp")
+dyn.load(dynlib("runHakeassessment"))
+library(r4ss)
+mod <- SS_output(paste(getwd(),'/data/SS32018', sep =''), printstats=FALSE, verbose = FALSE) # Read the true selectivity 
+
+# Set the seed
+seedz <- 12345
+set.seed(seedz)
+
+source('load_files.R')
+source('load_files_OM.R')
+source('run_agebased_model_true_catch_move.R')
+
+df <- load_data_seasons(nseason = 4, nspace = 2, bfuture = 0.5) # Prepare data for operating model
+#df
+
+
+parms.true <- getParameters_OM(TRUE,df) # Load parameters from assessment
+
+time <- 1
+yrinit <- df$nyear
+nruns <- 1000
+seeds <- floor(runif(n = nruns, min = 1, max = 1e6))
+### Run the OM and the EM for x number of years in the MSE 
+### Set targets for harvesting etc 
+#
+
+simyears <- 30 # Project 30 years into the future (2048 that year)
+year.future <- c(df$years,(df$years[length(df$years)]+1):(df$years[length(df$years)]+simyears))
+N0 <- NA
+sim.data <- run.agebased.true.catch(df) # Run the operating model until 2018
+
+simdata0 <- sim.data # The other one is gonna get overwritten. 
+
+# ### Loop MSE's with different errors in future survey and recruitment
+ls.save <- list()
+ls.converge <- matrix(0, nruns)
+
+df$nsurvey=1
+#
+for (i in 1:nruns){
+  tmp <- run_multiple_MSEs(simyears = simyears,
+                           seeds = seeds[i],
+                           TAC = 1, df = df)
+  #tmp <- run_multiple_MSEs(simyears = 30, seeds[i])
+  print(i)
+  
+  if(is.list(tmp)){
+    ls.save[[i]] <-tmp
+    ls.converge[i] <- 1
+  }else{
+    ls.save[[i]] <- NA
+    ls.converge[i] <- 0
+  }
+  
+  
+}
+# # # #
+save(ls.save,file = 'results/Survey/MSE_Real_Survey1.Rdata')
+
+### Loop MSE's with different errors in future survey and recruitment
+ls.save <- list()
+ls.converge <- matrix(0, nruns)
+#
+df$nsurvey=2
+for (i in 1:nruns){
+  tmp <- run_multiple_MSEs(simyears = simyears, seeds[i],
+                           TAC = 1, df =df)
+  #tmp <- run_multiple_MSEs(simyears = 30, seeds[i])
+  print(i)
+  if(is.list(tmp)){
+    ls.save[[i]] <-tmp
+    ls.converge[i] <- 1
+  }else{
+    ls.save[[i]] <- NA
+    ls.converge[i] <- 0
+  }
+
+
+}
+# # # #
+save(ls.save,file = 'results/Survey/MSE_Real_Survey2.Rdata')
+
+ls.save <- list()
+ls.converge <- matrix(0, nruns)
+df$nsurvey=3
+
+for (i in 1:nruns){
+  tmp <- run_multiple_MSEs(simyears = simyears, seeds[i],
+                           TAC = 1, df =df)
+  #tmp <- run_multiple_MSEs(simyears = 30, seeds[i])
+  print(i)
+  #tmp <- run_multiple_MSEs(simyears = 30, seeds[i])
+  if(is.list(tmp)){
+    ls.save[[i]] <-tmp
+    ls.converge[i] <- 1
+  }else{
+    ls.save[[i]] <- NA
+    ls.converge[i] <- 0
+  }
+  
+  
+  
+}
+# # # # 
+save(ls.save,file = 'results/Survey/MSE_Real_Survey3.Rdata')
