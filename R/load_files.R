880f15b090ceaa78355fa45f7db79556df62a171
nissandjac@gmail.com <nissandjac@gmail.com>
2020-02-26 09:55:56 -0800

    major restructure for package

---
+++ R/load_files.R
@@ -0,0 +1,7 @@
+source('R/plotValues.R')
+source('R/getUncertainty.R')
+source('R/load_data.R')
+source('R/ylimits.R')
+source('R/getParameters.R')
+source('R/Check_Identifiable_vs2.R')
+#source('getParms_noAge.R')
